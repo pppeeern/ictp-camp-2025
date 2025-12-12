e4354f44a5a90080e222b99d0808b98c96a5f4da
pppeeern <kaoklongpeerawas@gmail.com>
2025-12-12 12:38:00 +0700

    create next

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "ICTP CAMP 2025: Pirate of the Cyberian",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col overflow-auto">
+      <div className="h-screen bg-gradient-to-b from-cyan-600 via-teal-500 to-teal-300"></div>
+    </div>
+  );
+}
